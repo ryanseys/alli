ffe151ea1ab50fbaa284bc4c9601964a48762ade
Ryan Seys <ryan@ryanseys.com>
2014-04-30 14:52:14 -0400

    Add pagination loop

--- alli.go
+++ alli.go
@@ -54,64 +54,73 @@ func main() {
   }
 
   client := &http.Client{}
-  req, _ := http.NewRequest("GET", "https://api.github.com/users/" + username + "/repos?per_page=100", nil)
-  req.Header.Set("Accept", "application/vnd.github.v3+json")
+  var anotherPage bool = true
+  for anotherPage {
+    req, _ := http.NewRequest("GET", "https://api.github.com/users/" + username + "/repos?per_page=100", nil)
+    req.Header.Set("Accept", "application/vnd.github.v3+json")
 
-  if token != "" {
-    req.SetBasicAuth(token, "x-oauth-basic") // user, password
-  }
+    if token != "" {
+      req.SetBasicAuth(token, "x-oauth-basic") // user, password
+    }
 
-  repos, err := client.Do(req)
-  check(err)
+    repos, err := client.Do(req)
+    check(err)
 
-  defer repos.Body.Close()
-  contents, err := ioutil.ReadAll(repos.Body);
-  check(err)
+    defer repos.Body.Close()
+    contents, err := ioutil.ReadAll(repos.Body);
+    check(err)
 
-  byt := []byte(contents)
+    byt := []byte(contents)
 
-  var f interface{}
-  err = json.Unmarshal(byt, &f)
-  check(err)
+    var f interface{}
+    err = json.Unmarshal(byt, &f)
+    check(err)
 
-  array := f.([]interface {})
+    array := f.([]interface {})
 
-  println()
+    if(len(array) == 100) {
+      anotherPage = true
+    } else {
+      anotherPage = false
+    }
 
-  for i := range array {
-    repo := array[i].(map[string]interface {})
-    var countFloat float64 = repo["open_issues_count"].(float64)
-    var countInt int = int(countFloat)
-    if(countInt != 0) {
-      var name string = repo["full_name"].(string)
-      fmt.Printf("%s\n", name)
+    println()
 
-      req, _ = http.NewRequest("GET", "https://api.github.com/repos/" + name + "/issues?state=open", nil)
-      req.Header.Set("Accept", "application/vnd.github.v3+json")
-      if token != "" {
-        req.SetBasicAuth(token, "x-oauth-basic") // user, password
+    for i := range array {
+      repo := array[i].(map[string]interface {})
+      var countFloat float64 = repo["open_issues_count"].(float64)
+      var countInt int = int(countFloat)
+      if(countInt != 0) {
+        var name string = repo["full_name"].(string)
+        fmt.Printf("%s\n", name)
+
+        req, _ = http.NewRequest("GET", "https://api.github.com/repos/" + name + "/issues?state=open", nil)
+        req.Header.Set("Accept", "application/vnd.github.v3+json")
+        if token != "" {
+          req.SetBasicAuth(token, "x-oauth-basic") // user, password
+        }
+        issues, err := client.Do(req)
+        check(err)
+
+        defer issues.Body.Close()
+        iss, err := ioutil.ReadAll(issues.Body);
+        check(err)
+
+        byt = [] byte(iss)
+        var g interface{}
+        err = json.Unmarshal(byt, &g)
+        check(err)
+
+        issue_array := g.([]interface {})
+
+        for j := range issue_array {
+          issue := issue_array[j].(map[string]interface {})
+          var number int = int(issue["number"].(float64))
+          var title string = issue["title"].(string)
+          fmt.Printf("#%d %s\n", number, title)
+        }
+        fmt.Printf("\n");
       }
-      issues, err := client.Do(req)
-      check(err)
-
-      defer issues.Body.Close()
-      iss, err := ioutil.ReadAll(issues.Body);
-      check(err)
-
-      byt = [] byte(iss)
-      var g interface{}
-      err = json.Unmarshal(byt, &g)
-      check(err)
-
-      issue_array := g.([]interface {})
-
-      for j := range issue_array {
-        issue := issue_array[j].(map[string]interface {})
-        var number int = int(issue["number"].(float64))
-        var title string = issue["title"].(string)
-        fmt.Printf("#%d %s\n", number, title)
-      }
-      fmt.Printf("\n");
     }
   }
 }
